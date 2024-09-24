50626595424d0ed716cf8a9122abaf33da728281
cursedcadaver <collinschacha@hotmail.com>
2024-09-24 16:02:42 +0300

    I am genius

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,44 @@
+return {
+    -- Add Harpoon plugin
+    {
+        "ThePrimeagen/harpoon",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("harpoon").setup({
+                -- Optionally customize Harpoon settings here
+            })
+            -- Keybindings for Harpoon
+            local mark = require("harpoon.mark")
+            local ui = require("harpoon.ui")
+            vim.keymap.del("n", "<leader>e")
+
+            vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Harpoon Add File" })
+            vim.keymap.set("n", "<leader>e", ui.toggle_quick_menu, { desc = "Harpoon Menu" })
+            vim.keymap.set("n", "<leader>1", function()
+                ui.nav_file(1)
+            end, { desc = "Harpoon File 1" })
+            vim.keymap.set("n", "<leader>2", function()
+                ui.nav_file(2)
+            end, { desc = "Harpoon File 2" })
+            vim.keymap.set("n", "<leader>3", function()
+                ui.nav_file(3)
+            end, { desc = "Harpoon File 3" })
+            vim.keymap.set("n", "<leader>4", function()
+                ui.nav_file(4)
+            end, { desc = "Harpoon File 4" })
+        end,
+        lazy = false, -- Load Harpoon immediately (or configure with an event if needed)
+    },
+
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        opts = {
+            -- Remove the default keymap for <leader>e
+            window = {
+                mappings = {
+                    ["e"] = "",
+                },
+            },
+        },
+    },
+}
